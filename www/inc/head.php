510a466378e07aa50ccc3b70c8d99cadc43010d9
David Singleton <davidsingleton@david-3.office.last.fm>
2009-04-08 17:11:50 +0100

    Use non minified, and up to date hosted jquery

--- www/inc/head.php
+++ www/inc/head.php
@@ -1,6 +1,8 @@
 <title>Spiffdar | Playlists for Playdar</title>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/prototype/1.6/prototype.js"></script>
 <script type="text/javascript" src="/static/deps/playdar.js"></script>
 <script type="text/javascript" src="/static/deps/soundmanager2-nodebug-jsmin.js"></script>
 <script type="text/javascript" src="/static/spiffdar.js"></script>
+
 <link rel="stylesheet" href="/static/main.css" type="text/css" />
